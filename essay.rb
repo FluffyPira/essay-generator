ea548c527ce5ed150f1c7888426a591e4c35c38f
Pira Wetton <FluffyPira@users.noreply.github.com>
2014-11-11 20:32:58 -0500

    beacuse Marie was like 'stop sucking at ruby'

--- essay.rb
+++ essay.rb
@@ -7,27 +7,35 @@ WORDS = 15..30 # per sentence
 SENTENCES = 8..15 # per paragraph
 PARAGRAPHS = 5..7 # per essay
 
-def sentence_generate(number)
-  essay = MarkyMarkov::TemporaryDictionary.new(4)
-  essay.parse_file DICTIONARY
-  essay.generate_n_words number
-end
+class Essay
+  
+  def word_generate(number)
+    essay = MarkyMarkov::TemporaryDictionary.new(4)
+    essay.parse_file DICTIONARY
+    essay.generate_n_words number
+  end
 
-def paragraph_generate(sentences=1)
-  rand = rand(WORDS)
-  sentences.times.map { |x| sentence_generate(rand).capitalize }
-end
+  def sentence_generate(sentences=1)
+    words = rand(WORDS)
+    sentences.times.map { |x| word_generate(words).capitalize }
+  end
 
-def essay_generate
-  rand = rand(SENTENCES)
-  words = paragraph_generate(rand)
-  string = words.join(". ")
-  string.capitalize
-end
+  def paragraph_generate
+    sentences = rand(SENTENCES)
+    words = sentence_generate(sentences)
+    string = words.join(". ")
+    string.capitalize
+  end
+  
+  def generate
+    rand(PARAGRAPHS).times.map { paragraph_generate + ".\n\n" }
+  end
     
-randnum = rand(10000000..99999999)
-final = File.new("essays/essay-#{randnum}.txt", "w+")
-essay = rand(PARAGRAPHS).times.map { essay_generate + ".\n\n" }
-final.puts essay
-final.close
-puts "Output: Essay-#{randnum}.txt"
+end
+
+randnum  = rand(10000000..99999999)
+filename = "essays/essay-#{randnum}.txt"
+
+File.open(filename, "a+") {|f| f.puts Essay.new.generate}
+
+puts "Output: #{filename}"
