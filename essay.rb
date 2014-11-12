3e5e7847c1613f702dacdfea3687a4c2036dc131
Pira Wetton <FluffyPira@users.noreply.github.com>
2014-11-11 21:05:20 -0500

    Is now a module, thanks Marie

--- essay.rb
+++ essay.rb
@@ -7,35 +7,42 @@ WORDS = 15..30 # per sentence
 SENTENCES = 8..15 # per paragraph
 PARAGRAPHS = 5..7 # per essay
 
-class Essay
-  
-  def word_generate(number)
-    essay = MarkyMarkov::TemporaryDictionary.new(4)
-    essay.parse_file DICTIONARY
-    essay.generate_n_words number
-  end
+module EssayMaker
 
-  def sentence_generate(sentences=1)
-    words = rand(WORDS)
-    sentences.times.map { |x| word_generate(words).capitalize }
-  end
-
-  def paragraph_generate
-    sentences = rand(SENTENCES)
-    words = sentence_generate(sentences)
-    string = words.join(". ")
-    string.capitalize
-  end
+  class Essay
   
-  def generate
-    rand(PARAGRAPHS).times.map { paragraph_generate + ".\n\n" }
-  end
+    def initialize
+      @essay = MarkyMarkov::TemporaryDictionary.new(4)
+      @essay.parse_file DICTIONARY
+    end
     
+    def word_generate(number)
+      @essay.generate_n_words number
+    end
+
+    def sentence_generate(sentences=1)
+      words = rand(WORDS)
+      sentences.times.map { |x| word_generate(words).capitalize }
+    end
+
+    def paragraph_generate
+      sentences = rand(SENTENCES)
+      words = sentence_generate(sentences)
+      string = words.join(". ")
+      string.capitalize
+    end
+  
+    def generate
+      rand(PARAGRAPHS).times.map { paragraph_generate + ".\n\n" }
+    end
+    
+  end
+
 end
 
 randnum  = rand(10000000..99999999)
 filename = "essays/essay-#{randnum}.txt"
 
-File.open(filename, "w+") {|f| f.puts Essay.new.generate}
+File.open(filename, "w+") {|f| f.puts EssayMaker::Essay.new.generate}
 
 puts "Output: #{filename}"
